38548452457189b4526873599bf38e88dd358b35
LeslieJG <lgelberger@gmail.com>
2016-04-05 18:11:08 +1200

    Create Test Utilities modelled after Sunshine TestUtilities.java

---
+++ app/src/test/java/com/gmail/lgelberger/popularmovies/FullTestSuite.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gmail.lgelberger.popularmovies;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/*
+Boiler plate code that can be copied to any projects test
+directory and will run all tests in its package
+This entire class can be copied to any projects test folder
+and it should be able to run ALL the tests
+ */
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
