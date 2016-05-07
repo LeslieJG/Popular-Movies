c07c153011940475af5ce74391533e9aec0e0665
LeslieJG <lgelberger@gmail.com>
2016-05-08 11:48:14 +1200

    Add TestUriMatcher to test the URI Matcher in the Movie Content Provider

---
+++ app/src/androidTest/java/com/gmail/lgelberger/popularmovies/data/TestUriMatcher.java
@@ -0,0 +1,37 @@
+package com.gmail.lgelberger.popularmovies.data;
+
+import android.content.UriMatcher;
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Leslie on 2016-05-08.
+ * To Test UriMatcher in MovieProvider (the Content Provider for Movies)
+ */
+public class TestUriMatcher extends AndroidTestCase {
+
+    private static final long TEST_MOVIE_ID = 12L;
+
+    //content://com.gmail.lgelberger.popularmovies/movie
+    private static final Uri TEST_MOVIE_DIR = MovieContract.MovieEntry.CONTENT_URI;
+    private static final Uri TEST_MOVIE_WITH_ID_DIR = MovieContract.MovieEntry.buildMovieUriWithAppendedID(TEST_MOVIE_ID);
+
+
+    /*
+        This function tests that your UriMatcher returns the correct integer value
+        for each of the Uri types that the ContentProvider can handle.
+     */
+
+    public void testUriMatcher() {
+        UriMatcher testMatcher = MovieProvider.buildUriMatcher();
+
+
+        assertEquals("Error: The MOVIE URI was matched incorrectly.",
+                testMatcher.match(TEST_MOVIE_DIR), MovieProvider.MOVIE);
+        assertEquals("Error: The MOVIE WITH ID URI was matched incorrectly.",
+                testMatcher.match(TEST_MOVIE_WITH_ID_DIR), MovieProvider.MOVIE_DETAIL);
+       
+    }
+
+
+}
