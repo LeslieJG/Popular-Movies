6e4a241729a342352f072edfa4a5f1d574ca6dbe
LeslieJG <lgelberger@gmail.com>
2016-06-12 20:41:47 +1200

    Add Toolbar to Settings Menu with Up action enabled

--- app/src/main/java/com/gmail/lgelberger/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/gmail/lgelberger/popularmovies/SettingsActivity.java
@@ -1,7 +1,8 @@
 package com.gmail.lgelberger.popularmovies;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Leslie on 2016-03-08.
@@ -11,15 +12,23 @@ import android.os.Bundle;
  *
  */
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity { //changed from extends Activity to allow to action bar   AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //below needed for toolbar
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); //making a toolbar
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //allows up button
+
         // Display the SettingsFragment as the main content.
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+               // .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.movie_setting_container, new SettingsFragment())
                 .commit();
     }
 }
